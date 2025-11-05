9f4cf987f8fe86e979de540b2531680741f9e730
EricoSoares <61255002+EricoSoaress@users.noreply.github.com>
2025-11-05 11:29:23 -0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,26 +6,24 @@ from sqlalchemy.orm import sessionmaker
 import os
 from dotenv import load_dotenv
 
-# Carrega as variáveis de ambiente do arquivo .env (se ele existir)
+
 load_dotenv()
 
-# Pega a URL do banco de dados a partir das variáveis de ambiente
+
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Se a URL começar com "postgres://", substitui por "postgresql://"
-# A Heroku usa "postgres://", mas o SQLAlchemy espera "postgresql://"
+
 if SQLALCHEMY_DATABASE_URL and SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-# Caso a variável de ambiente não seja encontrada (para desenvolvimento local)
-# usamos um banco de dados SQLite.
+.
 if not SQLALCHEMY_DATABASE_URL:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
-# Cria a "engine" do banco de dados
+
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    # O argumento 'connect_args' só é necessário para SQLite.
+  
     connect_args={"check_same_thread": False} if "sqlite" in SQLALCHEMY_DATABASE_URL else {}
 )
 
